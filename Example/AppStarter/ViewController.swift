257db6d0a688c7353318d8f3b68cda3b76421fc1
Wilson Kim <wilsondgk@gmail.com>
2020-05-19 16:35:02 -0300

    Initial commit

---
+++ Example/AppStarter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppStarter
+//
+//  Created by atacadapp on 05/19/2020.
+//  Copyright (c) 2020 atacadapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
